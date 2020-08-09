a5a49324f2eea545f69f0a964441aa8170b77e89
GhulamYasinTahir <66848058+GhulamYasinTahir@users.noreply.github.com>
2020-08-09 11:12:04 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About Us'
 import DenseAppBar from './NavBar'
+import shoes from './components/ShoeArray'
 import { Link, BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 
 
@@ -65,22 +66,3 @@ const LaunchShoe=()=>{
   </div>
 )}
 
-///////////////////////////////////////////////////////
-
-const shoes = {
-    "air-jordan-3-valor-blue": {
-      name: "VALOUR BLUE",
-      img:
-        "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-    },
-    "jordan-mars-270-london": {
-      name: "JORDAN MARS 270 LONDON",
-      img:
-        "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-    },
-    "air-jordan-1-zoom-racer-blue": {
-      name: "RACER BLUE",
-      img:
-        "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-    }
-  };
